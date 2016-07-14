0f57c66b9cf1180a9f9771bf3d94481c13715e48
Ed Bartholomew <edmbartholomew@gmail.com>
2016-07-13 23:54:09 -0400

    forgot to check in change to attribute file - adding attributes for ports rather than hardcoding

--- cookbooks/redis-cluster/attributes/default.rb
+++ cookbooks/redis-cluster/attributes/default.rb
@@ -1,2 +1,4 @@
-default['redis-cluster']['slave'] = nil
 default['redis-cluster']['master-ip'] = nil
+default['redis-cluster']['slave'] = false
+default['redis-cluster']['redis-port'] = 6379
+default['redis-cluster']['sentinel-port'] = 16379
